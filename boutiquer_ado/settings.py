0e65d6c9fb10fd67d079abc005bea6b416945861
RussOakham <rjoakham@gmail.com>
2021-05-28 08:50:03 +0000

    Added add to bag functionality

--- boutiquer_ado/settings.py
+++ boutiquer_ado/settings.py
@@ -71,7 +71,8 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # Required by Allauth
+                # Required by Allauth
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
