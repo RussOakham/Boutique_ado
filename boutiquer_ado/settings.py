ea801da5861d6ac5235ab32b479f82fd107555cf
RussOakham <rjoakham@gmail.com>
2021-05-30 15:31:50 +0000

    added checkout views and templates

--- boutiquer_ado/settings.py
+++ boutiquer_ado/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 
@@ -60,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutiquer_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,8 +81,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
--- checkout/signals.py
+++ checkout/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from .models import OrderLineItem
 
 
-@reciever(post_save, sender=OrderLineItem)
+@receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
@@ -12,7 +12,7 @@ def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
 
-@reciever(post_delete, sender=OrderLineItem)
+@receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
