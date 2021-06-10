fc98e0b362a37633f991c430d64a00ee1617c370
RussOakham <rjoakham@gmail.com>
2021-06-10 11:33:11 +0000

    deployment - procfile and allowed host setup

--- boutiquer_ado/settings.py
+++ boutiquer_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0xr5+p_%9wo$^g2l%sdxz=!ixjrubc$4y%pyx!=+m_ge8dx3n4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-ado-rjo.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +126,17 @@ WSGI_APPLICATION = 'boutiquer_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
