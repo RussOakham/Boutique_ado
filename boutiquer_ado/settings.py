978b64c2402ea7f073451509d8c0e452d290b690
RussOakham <rjoakham@gmail.com>
2021-06-10 15:13:16 +0000

    deployment - updated secret key and debug

--- boutiquer_ado/settings.py
+++ boutiquer_ado/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0xr5+p_%9wo$^g2l%sdxz=!ixjrubc$4y%pyx!=+m_ge8dx3n4'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-rjo.herokuapp.com', 'localhost']
 
